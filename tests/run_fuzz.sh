fb5e376dd480fffa5bcfc6855eae7aa0870b7749
killuayz <2959243019@qq.com>
2024-05-25 09:13:30 +0000

    add run_fuzz.sh

---
+++ tests/run_fuzz.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: $0 <target program> <cmdline>"
+    exit 1
+fi
+
+TARGET=$1
+INPUT_PATH=/benchmark/seed/$TARGET
+OUTPUT_PATH=/tests/$1
+TAINT_PROG=/benchmark/bin/Angora/track/$1
+FAST_PROG=/benchmark/bin/Angora/fast/$1
+ARGS=$2
+echo TAINT_PROG=$TAINT_PROG
+echo FAST_PROG=$FAST_PROG
+/Angora/angora_fuzzer -i $INPUT_PATH -o $OUTPUT_PATH -t $TAINT_PROG -- $FAST_PROG $ARGS
